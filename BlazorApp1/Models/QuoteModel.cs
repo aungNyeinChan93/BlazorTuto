8c4e0b65a6a5852d5ceb5a2eedf22301f59c08e6
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-04-24 00:01:47 +0630

    Add project files.

---
+++ BlazorApp1/Models/QuoteModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp1.Models
+{
+    public class QuoteModel
+    {
+        public List<Quote> quotes { get; set; }
+
+    }
+
+    public class Quote
+    {
+        public int id { get; set; }
+        public string? quote { get; set; }
+        public string? author { get; set; }
+    }
+
+}
---
+++ BlazorApp1/Services/QuoteService.cs
@@ -0,0 +1,34 @@
+﻿using BlazorApp1.Models;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace BlazorApp1.Services
+{
+    public class QuoteService
+    {
+
+        private readonly RestClient _restClient;
+
+        public QuoteService(RestClient restClient)
+        {
+            _restClient = restClient;
+        }
+
+        public async Task<QuoteModel?> GetAllQuotes(string url)
+        {
+            var request = new RestRequest(url,Method.Get);
+            var response = await _restClient.ExecuteAsync(request);
+            if (!response.IsSuccessful)
+            {
+                return default!;
+            }
+            var str =  response.Content;
+            if (str is null)
+            {
+                return default!;
+            }
+            var quotes = JsonConvert.DeserializeObject<QuoteModel?>(str);
+            return quotes;
+        }
+    }
+}
